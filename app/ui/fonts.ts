3236ce8809ee6c1b80944c0deb5670b7b371fcf1
tharsis-soares <tharsissoares@hotmail.com>
2024-11-15 10:28:56 -0300

    'first-commit'

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Roboto, Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']})
+
+export const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
